1238e420f662113d69122bcfb1b823338d7eda5d
diabloktml3dr <diabloktml3dr@gmail.com>
2024-12-30 10:16:06 +0700

    first commit

---
+++ javascrip.js
@@ -0,0 +1,36 @@
+// Bắt sự kiện nhấn nút menu
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show'); // Thêm/gỡ class 'show' để bật/tắt menu
+});
+
+
+// sự kiện trượt
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
